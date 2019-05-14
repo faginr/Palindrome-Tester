4c03ccc3079bc9e761d8e98007a6f8c356d54bc8
Rex Fagin <rexwfagin@gmail.com>
2019-05-14 14:10:42 -0700

    initial logic finished. Next move on to HTML interface.

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 function palindrome(str) {
-  //Convert String to Array
-  let arr = str.split("");
+  //Convert String to Lower case
+  let newStr = str.toLowerCase();
+  //Remove non-alphanumeric
+  newStr = newStr.replace(/[^a-z^0-9]/g, "");
+  //Convert string to array
+  let arr = newStr.split("");
   //Determine if Array is even
   if (arr.length % 2 === 0) {
     //even
     let frontEven = 0;
     let backEven = (arr.length) - 1;
+    //compare element by element
     for (let i = 0; i<arr.length/2; i++){
-      console.log("frontEven " + frontEven);
-      console.log("backEven " + backEven);
-      console.log("i: " + i);
+      let frontTemp = arr[frontEven];
+      let backTemp = arr[backEven];
+      //determine matches
+      if (frontTemp === backTemp){
+        console.log (frontTemp + " = " + backTemp);
+        console.log(true);
+      } else {
+        console.log(frontTemp + " != " + backTemp);
+        console.log(false);
+        //is not a palindrome, exit loop
+        return false;
+      }
       frontEven += 1;
       backEven -= 1;
     }
+    return true
   } else {
+    //odd
+    //repeat steps for odd case
     let frontOdd= 0;
     let backOdd = (arr.length) - 1;
+    //modify math for odd case
     for (let i = 0; i<arr.length/2; i++){
-      console.log("frontOdd " + frontOdd);
-      console.log("backOdd " + backOdd);
-      console.log("i: " + i);
+      let frontTemp = arr[frontOdd];
+      let backTemp = arr[backOdd];
+       if (frontTemp === backTemp){
+        console.log (frontTemp + " = " + backTemp);
+        console.log(true);
+      } else {
+        console.log(frontTemp + " != " + backTemp);
+        console.log(false);
+        return false;
+      }
       frontOdd += 1;
       backOdd -= 1;
     }
-  }
-  //create loop to test first and last, next...
-  //same for odd case
-  //return true or false
-  return true;
+    return true
+}
 }
 
 
-
-palindrome("eye");
+palindrome("A man, A plan, a canal. Panama");
